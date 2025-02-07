ede4b7f56b4a3c8cdd0c2a74d10fba0d8b3098cf
Alvaro Aleman <alvaroaleman@users.noreply.github.com>
2025-02-07 12:20:41 -0500

    Allow to ignore teams

--- main.go
+++ main.go
@@ -16,8 +16,9 @@ import (
 func main() {
 	cmd := &cobra.Command{}
 	authorsToIgnore := cmd.Flags().StringArray("authors-to-ignore", nil, "Authors to ignore")
+	teamstoIgnore := cmd.Flags().StringArray("teams-to-ignore", nil, "Teams to ignore")
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
-		return run(cmd.Context(), sets.New(*authorsToIgnore...))
+		return run(cmd.Context(), sets.New(*authorsToIgnore...), sets.New(*teamstoIgnore...))
 	}
 
 	if err := cmd.Execute(); err != nil {
@@ -26,20 +27,31 @@ func main() {
 	}
 }
 
-func run(ctx context.Context, authorsToIgnore sets.Set[string]) error {
+func run(ctx context.Context, authorsToIgnore, teamstoIgnore sets.Set[string]) error {
 	l, err := zap.NewDevelopment()
 	if err != nil {
 		return fmt.Errorf("failed to get logger: %w", err)
 	}
 	client := github.NewClient(nil).WithAuthToken(os.Getenv("GITHUB_TOKEN"))
+
+	userResp, _, err := client.Users.Get(ctx, "")
+	if err != nil {
+		return fmt.Errorf("failed to get current user: %w", err)
+	}
+	currentUser := userResp.GetLogin()
+
 	result, _, err := client.Activity.ListNotifications(ctx, &github.NotificationListOptions{})
 	if err != nil {
 		return fmt.Errorf("failed to list github notifications: %w", err)
 	}
 	l.Sugar().Infof("got %d notifications", len(result))
 
-	var toMarkRead []string
+	var toMarkRead []*github.Notification
 	for _, n := range result {
+		// Always allow explicit mention
+		if *n.Reason != "review_requested" {
+			continue
+		}
 		if n.Subject != nil && n.Subject.Type != nil && *n.Subject.Type == "PullRequest" {
 			urlPieces := strings.Split(n.Subject.GetURL(), "/")
 			numberS := urlPieces[len(urlPieces)-1]
@@ -53,16 +65,39 @@ func run(ctx context.Context, authorsToIgnore sets.Set[string]) error {
 			}
 			author := strings.TrimSuffix(*pr.User.Login, "[bot]")
 			if authorsToIgnore.Has(author) {
-				toMarkRead = append(toMarkRead, *n.ID)
+				toMarkRead = append(toMarkRead, n)
+				continue
+			}
+
+			events, _, err := client.Issues.ListIssueEvents(ctx, n.Repository.GetOwner().GetLogin(), n.Repository.GetName(), number, &github.ListOptions{})
+			if err != nil {
+				return fmt.Errorf("failed to fetch events for PR %d for repository %q: %w", number, n.Repository.GetFullName(), err)
+			}
+			var requstedIgnoredTeam, requestedUser bool
+			for _, event := range events {
+				if *event.Event != "review_requested" {
+					continue
+				}
+				if event.RequestedTeam != nil && teamstoIgnore.Has(*event.RequestedTeam.Name) {
+					requstedIgnoredTeam = true
+				}
+				if event.RequestedReviewer != nil && *event.RequestedReviewer.Login == currentUser {
+					requestedUser = true
+				}
+			}
+
+			if requstedIgnoredTeam && !requestedUser {
+				toMarkRead = append(toMarkRead, n)
 			}
 		}
 	}
 
 	l.Sugar().Infof("marking %d notifications as read", len(toMarkRead))
-	for _, id := range toMarkRead {
-		if _, err := client.Activity.MarkThreadRead(ctx, id); err != nil {
-			return fmt.Errorf("failed to mark notification %q as read: %w", id, err)
+	for _, notification := range toMarkRead {
+		if _, err := client.Activity.MarkThreadRead(ctx, *notification.ID); err != nil {
+			return fmt.Errorf("failed to mark notification %q as read: %w", *notification.ID, err)
 		}
+		l.Sugar().Info("marked notification as read", *notification.Subject.URL)
 	}
 
 	return nil
